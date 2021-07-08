eab9ed4bddd48e7c6b80cf7e1050afba5d9105e5
Do Anh Vu <vufabio@gmail.com>
2021-07-08 16:52:10 +0700

    vuda commit 20210708

--- Document/SQL API/EndUserRequest_Detail_Payment.sql
+++ Document/SQL API/EndUserRequest_Detail_Payment.sql
@@ -1,6 +1,7 @@
 ﻿SELECT * FROM enterprise_payments ep;
 SELECT * FROM enterprise_refer_payments erp;
-SELECT * FROM payonline_transaction pt;
+SELECT * FROM payonline_transaction pt WHERE pt.order_id=2;
 SELECT * FROM pay_transaction pt;
 SELECT * FROM payment p;
 SELECT * FROM product_genre pg;
+SELECT * FROM enterprise_payment_issues epi;
